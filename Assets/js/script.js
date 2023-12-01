02ec1045229ce59bbb3fe8d8a99b5c6ddd4234f6
Shelby Hutchinson <shhutc18@gmail.com>
2023-11-30 18:06:41 -0800

    added JS for current day forecast functionality

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -57,19 +57,28 @@ searchButton.click(function() {
 // All of the above will let you search for a city and save it to your application.
 // The following will allow you to re-click on previously searched cities and display the weather again.
 $(document).on('click', '#city-list li', function() {
-    // Get the city that the user has clicked on
     var city = $(this).text();
 
-    // Fetch the weather data for the city and display it
     fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey)
         .then(function(response) {
             return response.json();
         })
         .then(function(data) {
-            // Extract the weather data from the response
             var weather = data.weather[0].description;
+            var iconUrl = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+            var temperature = data.main.temp;
+            var humidity = data.main.humidity;
+            var windSpeed = data.wind.speed;
 
-            // Display the weather data in the custom-box div
-            customBox.text(weather);
+            var date = new Date();
+            var formattedDate = (date.getMonth()+1) + '/' + date.getDate() + '/' + date.getFullYear();
+
+            customBox.empty();
+
+            customBox.append('<h2>' + city + ' (' + formattedDate + ')' + '</h2>');
+            customBox.append('<img src="' + iconUrl + '" alt="Weather icon">');
+            customBox.append('<p>Temp: ' + temperature + '°F</p>');
+            customBox.append('<p>Humidity: ' + humidity + '%</p>');
+            customBox.append('<p>Wind Speed: ' + windSpeed + ' MPH</p>');
         });
 });
