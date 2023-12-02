ddebab3db84d3853f8ca4a426493349e4abc0c37
Shelby Hutchinson <shhutc18@gmail.com>
2023-12-01 16:20:54 -0800

    final comments on JS file

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -105,19 +105,12 @@ $(document).on('click', '#city-list li', function() {
             var weather = data.weather[0].description;
             var iconUrl = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
             var temperature = data.main.temp;
-
-            // Convert the temperature from Kelvin to Fahrenheit
             var temperature = Math.round(data.main.temp * 9/5 - 459.67);
-
             var humidity = data.main.humidity;
             var windSpeed = data.wind.speed;
-
-            // Create a new Date object using Day.js
             var date = dayjs();
             var formattedDate = date.format('M/D/YYYY');
-            
             customBox.empty();
-
             customBox.append('<h2>' + city + ' (' + formattedDate + ')' + '</h2>');
             customBox.append('<img src="' + iconUrl + '" alt="Weather icon">');
             customBox.append('<p>Temp: ' + temperature + '°F</p>');
@@ -130,29 +123,17 @@ $(document).on('click', '#city-list li', function() {
             return response.json();
         })
         .then(function(data) {
-            // Loop through the list of forecasts
             for (var i = 0; i < data.list.length; i += 8) {
                 var forecast = data.list[i];
-
                 var date = new Date(forecast.dt * 1000);
                 var formattedDate = (date.getMonth()+1) + '/' + date.getDate() + '/' + date.getFullYear();
-
                 var iconUrl = "http://openweathermap.org/img/w/" + forecast.weather[0].icon + ".png";
                 var temperature = forecast.main.temp;
-
-                // Convert the temperature from Kelvin to Fahrenheit
                 var temperature = Math.round(forecast.main.temp * 9/5 - 459.67);
-
                 var humidity = forecast.main.humidity;
                 var windSpeed = forecast.wind.speed;
-
-                // Select the card for the day
                 var card = $('.card').eq(i / 8);
-
-                // Clear the card
                 card.empty();
-
-                // Append the forecast to the card
                 card.append('<h5 class="card-title">' + formattedDate + '</h5>');
                 card.append('<img class="weather-icon" src="' + iconUrl + '" alt="Weather icon">');
                 card.append('<p>Temp: ' + temperature + '°F</p>');
